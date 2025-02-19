c9ca9f3212981d11174190c6103dc9f073d3ee10
/tecniccomsono <117489835+TecnicComSono@users.noreply.github.com>
2025-02-19 19:26:07 -0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -103,16 +103,12 @@ document.body.appendChild(script);
 (async () => {
     await loadCss('https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css');
     await loadScript('https://cdn.jsdelivr.net/npm/toastify-js', 'toastifyPlugin');
-
     showLoadingScreen();
     await delay(2500);
     playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/gcelzszy.wav');
-
-    loadedPlugins.forEach(plugin => sendToast(`🔌 ${plugin} Started!`, 2000, 'top'));
-
     hideLoadingScreen();
     sendToast("📎 Khanclient injected!", 3000, "bottom");
     sendToast("⭐ Made by FontesCode", 3000, "bottom");
-
+    loadedPlugins.forEach(plugin => sendToast(`🔌 ${plugin} Started!`, 3000, 'bottom'));
     console.log("Made by FontesCode")
 })().catch(error => console.error("Error ocurred:", error));
