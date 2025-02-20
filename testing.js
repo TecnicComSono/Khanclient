bb23801bc1e6010c940c30e566d44469cf22bf3a
/tecniccomsono <117489835+TecnicComSono@users.noreply.github.com>
2025-02-19 22:23:45 -0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,8 +1,51 @@
 // V0.1
 const version = "V1.0.0";
 
+// Patching
+
+const repoPath = "https://raw.githubusercontent.com/TecnicComSono/Khanclient/refs/heads/main/"
+
+// Device
+
+let device = {
+    mobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|Mobile|Tablet|Kindle|Silk|PlayBook|BB10/i.test(navigator.userAgent),
+    apple: /iPhone|iPad|iPod|Macintosh|Mac OS X/i.test(navigator.userAgent)
+};
+
 // Plugins archive
 const loadedPlugins = [];
+
+// Elements
+
+const unloader = document.createElement('unloader');
+const dropdownMenu = document.createElement('dropDownMenu');
+const watermark = document.createElement('watermark');
+const statsPanel = document.createElement('statsPanel');
+
+// /* Globals */
+window.features = {
+    questionSpoof: true,
+    videoSpoof: true,
+    showAnswers: false,
+    autoAnswer: false,
+    customBanner: false,
+    nextRecomendation: false,
+    repeatQuestion: false,
+    minuteFarmer: false,
+    rgbLogo: false
+};
+window.featureConfigs = {
+    autoAnswerDelay: 3,
+    customUsername: "",
+    customPfp: ""
+};
+
+// Emmiter
+
+class EventEmitter{constructor(){this.events={}}on(t,e){"string"==typeof t&&(t=[t]),t.forEach(t=>{this.events[t]||(this.events[t]=[]),this.events[t].push(e)})}off(t,e){"string"==typeof t&&(t=[t]),t.forEach(t=>{this.events[t]&&(this.events[t]=this.events[t].filter(t=>t!==e))})}emit(t,...e){this.events[t]&&this.events[t].forEach(t=>{t(...e)})}once(t,e){"string"==typeof t&&(t=[t]);let s=(...i)=>{e(...i),this.off(t,s)};this.on(t,s)}};
+const plppdo = new EventEmitter();
+
+new MutationObserver((mutationsList) => { for (let mutation of mutationsList) if (mutation.type === 'childList') plppdo.emit('domChanged'); }).observe(document.body, { childList: true, subtree: true });
 
 // Plugins functions
 async function loadScript(url, label) {
@@ -24,13 +67,15 @@ async function loadCss(url) {
 
 // Misc functions
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-const playAudio = url => {
-    const audio = new Audio(url);
-    audio.play();
-    console.log(`Playing ${url}`);
-};
+const playAudio = url => { const audio = new Audio(url); audio.play(); console.log(`Playing ${url}`); };
+const findAndClickByClass = className => { const element = document.querySelector(`.${className}`); if (element) { element.click(); sendToast(`⭕ Pressionando ${className}...`, 1000); } }
+
+// Modify elements
+
+document.head.appendChild(Object.assign(document.createElement('style'),{innerHTML:"::-webkit-scrollbar { width: 8px; } ::-webkit-scrollbar-track { background: #f1f1f1; } ::-webkit-scrollbar-thumb { background: #888; border-radius: 10px; } ::-webkit-scrollbar-thumb:hover { background: #555; }"}));
 
 // Loading element and function
+
 const LoadingScreen = document.createElement('div');
 LoadingScreen.style.cssText = "position:fixed;top:0;left:0;width:100%;height:100%;background-color:#000;display:flex;align-items:center;justify-content:center;z-index:9999;opacity:0;transition:opacity 1s ease;user-select:none;color:white;font-family:MuseoSans,sans-serif;font-size:30px;text-align:center;";
 LoadingScreen.innerHTML = '<span style="color:white;">KHAN</span><span style="color:#7f4fd1;">.CLIENT</span>';
@@ -98,6 +143,14 @@ const script = Object.assign(document.createElement("script"), {
     },
 });
 document.body.appendChild(script);
+
+// Visual Functions
+
+function setupMenu() {
+    loadScript(repoPath+'ui/mainMenu.js', 'mainMenu');
+    loadScript(repoPath+'ui/statusPanel.js', 'statusPanel');
+    loadScript(repoPath+'ui/devTab.js', 'devTab');
+}
 
 // Loading
 (async () => {
